fd794235190ee7d8ed95abb25c853575563675a5
Cristian Vasquez <cvo6372@gmail.com>
2023-10-30 01:18:57 -0500

    .env example

---
+++ src/database_source.ts
@@ -0,0 +1,31 @@
+import { Item } from './items/entities/item.entity';
+import { ItemsReceived } from './items_received/entities/items_received.entity';
+import 'dotenv/config'
+
+const databaseSource: { [key: string]: any }[] = [
+    {
+        name: process.env.NEST_DATABASE_CONNECTION,
+        type: 'mysql',
+        host: process.env.NEST_DATABASE_HOST,
+        username: process.env.NEST_DATABASE_USER,
+        password: process.env.NEST_DATABASE_PASSWORD,
+        port: 3306,
+        database: process.env.NEST_DATABASE_DB,
+        //entities: [__dirname + '/**/*.entity{.ts,.js}'],
+        entities: [Item],
+        synchronize: true
+    },
+    {
+        name: process.env.NESTDOCKER_DATABASE_CONNECTION,
+        type: 'mysql',
+        host: process.env.NESTDOCKER_DATABASE_HOST,
+        username: process.env.NESTDOCKER_DATABASE_USER,
+        password: process.env.NESTDOCKER_DATABASE_PASSWORD,
+        port: 3307,
+        database: process.env.NESTDOCKER_DATABASE_DB,
+        //entities: [__dirname + '/**/*.entity{.ts,.js}'],
+        entities: [ItemsReceived],
+        synchronize: true
+    }
+]
+export { databaseSource }
