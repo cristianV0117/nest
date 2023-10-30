ad4227a2e19ebd6e09c1e3d0664156410186be0c
Cristian Vasquez <cvo6372@gmail.com>
2023-10-30 10:40:26 -0500

    middleware

---
+++ src/middleware/api-key.middleware.ts
@@ -0,0 +1,16 @@
+import { HttpException, HttpStatus, Injectable, NestMiddleware } from '@nestjs/common';
+import { Request, Response, NextFunction } from 'express';
+import 'dotenv/config'
+
+@Injectable()
+export class ApiKeyMiddleware implements NestMiddleware {
+  use(req: Request, res: Response, next: NextFunction) {
+    if (undefined === req.headers.api_key) {
+        throw new HttpException("No existe el API KEY", HttpStatus.BAD_REQUEST)
+    }
+    if (req.headers.api_key != process.env.API_KEY) {
+        throw new HttpException("API KEY incorrecto", HttpStatus.BAD_REQUEST)
+    }
+    next()
+  }
+}
